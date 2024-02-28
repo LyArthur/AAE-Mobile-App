cb4bb340dd1fd4d87871fb6727356cf35723985d
Arthur Ly <Arthur.ly.flwrs@gmail.com>
2024-02-28 10:51:57 +0100

    refactoring et mise en mémoire des détails des utilisateurs

---
+++ src/screens/detailsUtilisateurScreen/components/contacts.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import {StyleSheet, TouchableOpacity, View, Text} from "react-native";
+import {Linking} from 'react-native';
+
+export const makePhoneCall = (phoneNumber) => {
+    Linking.openURL(`tel:${phoneNumber}`);
+};
+
+export const sendEmail = (email) => {
+    Linking.openURL(`mailto:${email}`);
+};
+
+export const RenderContacts = ({data}) => {
+    const contacts = [];
+    if (data.user_email) {
+        contacts.push(
+            <TouchableOpacity onPress={() => sendEmail(data.user_email)}>
+                <View style={styles.contactInfo}>
+                    <Text style={styles.contactLabel}>Email :</Text>
+                    <Text style={styles.contactValue}>{data.user_email}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    }
+    for (let i = 1; i < 6; i++) {
+        if (data[`type-tel-0${i}`] !== "" && data[`type-tel-0${i}`] !== undefined) {
+            contacts.push(
+                <TouchableOpacity onPress={() => makePhoneCall(data[`telephone-0${i}`])} key={`telephone-0${i}`}>
+                    <View style={styles.contactInfo}>
+                        <Text style={styles.contactLabel}>{data[`type-tel-0${i}`]} :</Text>
+                        <Text style={styles.contactValue}>{data[`telephone-0${i}`]}</Text>
+                    </View>
+                </TouchableOpacity>
+            );
+        }
+    }
+    return (
+        <View style={styles.contactContainer}>
+            <Text style={styles.sectionTitle}>Contacts :</Text>
+            {contacts}
+        </View>);
+};
+
+const styles = StyleSheet.create({
+    contactContainer: {
+        marginTop: 20,
+    },
+    sectionTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    contactInfo: {
+        flexDirection: 'row',
+        marginBottom: 10,
+    },
+    contactLabel: {
+        fontWeight: 'bold',
+        marginRight: 10,
+    },
+    contactValue: {
+        flex: 1,
+    },
+});
--- src/screens/detailsUtilisateurScreen/components/showDetails.jsx
+++ src/screens/detailsUtilisateurScreen/components/showDetails.jsx
@@ -1,74 +1,41 @@
 import React, { useEffect, useState } from "react";
-import { Linking, ScrollView, StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
+import { ScrollView, StyleSheet, Text, View, Image } from "react-native";
 import LoadingScreen from "../../loadingScreen";
 import { getDetails } from "../../../api/AAE_api";
+import {RenderContacts} from "./contacts";
+import {RenderProfileHeader} from "./profileHeader";
+
+const cachedData = {};
 
 export const ShowDetails = ({ id }) => {
     const [data, setData] = useState(null);
 
     useEffect(() => {
-        const getData = async () => {
-            const result = await getDetails(id);
-            if (result === false) {
-                return (
-                    <Text>No data</Text>
-                );
+        const fetchData = async () => {
+            if (cachedData[id]) {
+                setData(cachedData[id]);
+            } else {
+                const result = await getDetails(id);
+                if (result === false) {
+                    return (
+                        <Text>No data</Text>
+                    );
+                }
+                cachedData[id] = result.Data;
+                setData(result.Data);
             }
-            setData(result.Data);
         };
-        getData();
-    }, []);
+
+        fetchData();
+    }, [id]);
 
     if (data === null) {
         return <LoadingScreen />;
     }
-
-    const makePhoneCall = (phoneNumber) => {
-        Linking.openURL(`tel:${phoneNumber}`);
-    };
-
-    const sendEmail = () => {
-        const email = data.user_email;
-        Linking.openURL(`mailto:${email}`);
-    };
-
-    const numeroTelephone = [];
-    for (let i = 1; i < 6; i++) {
-        if (data[`type-tel-0${i}`] !== "" && data[`type-tel-0${i}`] !== undefined) {
-            numeroTelephone.push(
-                <TouchableOpacity onPress={() => makePhoneCall(data[`telephone-0${i}`])} key={`telephone-0${i}`}>
-                    <View style={styles.contactInfo}>
-                        <Text style={styles.contactLabel}>{data[`type-tel-0${i}`]} :</Text>
-                        <Text style={styles.contactValue}>{data[`telephone-0${i}`]}</Text>
-                    </View>
-                </TouchableOpacity>
-            );
-        }
-    }
-
     return (
         <ScrollView contentContainerStyle={styles.container}>
-            <View style={styles.profileHeader}>
-                <View style={styles.profileImageContainer}>
-                    <Image
-                        source={{ uri: data.img }}
-                        style={styles.profileImage}
-                    />
-                </View>
-                <Text style={styles.userName}>{data.first_name} {data.last_name}</Text>
-            </View>
-            <View style={styles.contactContainer}>
-                <Text style={styles.sectionTitle}>Contacts :</Text>
-                {numeroTelephone}
-                {data.user_email && (
-                    <TouchableOpacity onPress={sendEmail}>
-                        <View style={styles.contactInfo}>
-                            <Text style={styles.contactLabel}>Email :</Text>
-                            <Text style={styles.contactValue}>{data.user_email}</Text>
-                        </View>
-                    </TouchableOpacity>
-                )}
-            </View>
+            <RenderProfileHeader data={data}/>
+            <RenderContacts data={data}/>
         </ScrollView>
     );
 };
@@ -77,44 +44,6 @@ const styles = StyleSheet.create({
     container: {
         flexGrow: 1,
         padding: 20,
-    },
-    profileHeader: {
-        alignItems: 'center',
-        marginBottom: 20,
-    },
-    profileImageContainer: {
-        borderRadius: 50,
-        overflow: 'hidden',
-    },
-    profileImage: {
-        width: 100,
-        height: 100,
-        borderRadius: 75,
-    },
-    userName: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 10,
-        textAlign: 'center',
-    },
-    contactContainer: {
-        marginTop: 20,
-    },
-    sectionTitle: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginBottom: 10,
-    },
-    contactInfo: {
-        flexDirection: 'row',
-        marginBottom: 10,
-    },
-    contactLabel: {
-        fontWeight: 'bold',
-        marginRight: 10,
-    },
-    contactValue: {
-        flex: 1,
     },
 });
 
